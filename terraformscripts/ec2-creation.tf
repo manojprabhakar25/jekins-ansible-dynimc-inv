0cfc055247aa1980da559af64a9fe5c69095f42a
Manoj prabhakar j <104354494+manojprabhakar25@users.noreply.github.com>
2022-11-05 14:56:02 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,5 +1,5 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+   region  = "ap-south-1" 
    
 }
 
@@ -8,9 +8,9 @@ resource "aws_instance" "AWSEC2Instance"{
      ami     =  "ami-051f0947e420652a9"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     key_name        = "maven_server"
      tags = {
-        Name = "tomcatserver"
+        Name = "tomeey"
      }
 }
 
